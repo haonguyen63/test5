968266bbf911c5a4cee2f052a68fdb523acadc7b
haonguyen63 <101074056+haonguyen63@users.noreply.github.com>
2025-11-05 11:16:16 +0700

    Delete loyalty-render-ui5/next.config.mjs

--- loyalty-render-ui5/next.config.mjs
+++
@@ -1,9 +0,0 @@
-// next.config.js — patch: remove `output: "standalone"` to allow custom Express server
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // IMPORTANT: do NOT set output: "standalone" when using a custom Express server
-  // output: "standalone",
-};
-
-module.exports = nextConfig;
