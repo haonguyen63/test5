678829a73bef2cb4254de0d27171efe3f44b6de2
haonguyen63 <101074056+haonguyen63@users.noreply.github.com>
2025-11-05 12:17:32 +0700

    Delete loyalty-render-ui5/next.config.cjs

--- loyalty-render-ui5/next.config.cjs
+++
@@ -1,9 +0,0 @@
-// next.config.cjs — CommonJS config (works even if package.json has "type":"module")
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // DO NOT set output: "standalone" when using a custom Express server
-  // output: "standalone",
-};
-
-module.exports = nextConfig;
